a97887a58171e173b2025bc0b90be671f88a63ef
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2021-07-01 16:52:22 +0530

    style:added light and dark theme in Color.js

---
+++ my-app/src/Color.js
@@ -0,0 +1,12 @@
+const AppTheme={
+    light:{
+        textColor:"white",
+        backgroundColor:"black",
+        border:"2px solid #000"
+    },
+    dark:{
+        textColor:"#fff",
+        backgroundColor:"#333",
+        border:"2px dashed #000"
+    }
+}
